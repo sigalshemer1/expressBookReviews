7b894b695a8b80b2e4d076f0735c3d4c4ba16c28
Abdoulaye Bagayoko <155672513+Baga-Tim@users.noreply.github.com>
2024-03-20 11:18:11 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -6,6 +6,7 @@ const genl_routes = require('./router/general.js').general;
 public_users.get('/',function (req, res) { public_users.get('/isbn/:isbn',function (req, res) {
  public_users.get('/author/:author',function (req, res) {
   public_users.get('/title/:title',function (req, res) {
+   public_users.get('/review/:isbn',function (req, res) {
 const app = express();
 
 app.use(express.json());
