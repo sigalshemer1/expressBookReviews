3054d558d1aacd8986fa6b51ea19cd11db0407df
sigalshemer1 <sigal.shemer68@gmail.com>
2024-07-05 06:54:40 -0400

    express 1

--- final_project/index.js
+++ final_project/index.js
@@ -3,11 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-public_users.get('/',function (req, res) { 
- public_users.get('/isbn/:isbn',function (req, res) {
- public_users.get('/author/:author',function (req, res) {
-  public_users.get('/title/:title',function (req, res) {
-   public_users.get('/review/:isbn',function (req, res) {
+
 const app = express();
 
 app.use(express.json());
@@ -15,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "Customer not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "Customer not logged in"})
+     }
 });
  
 const PORT =5000;
