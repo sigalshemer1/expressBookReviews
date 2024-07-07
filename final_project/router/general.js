df7609d7242726eecdbec4b25d76374fa270301a
sigalshemer1 <sigal.shemer68@gmail.com>
2024-07-07 17:03:39 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,6 +67,28 @@ public_users.get('/', async function (req, res) {
     }
   });
 
+  
+// Get book details based on author
+public_users.get('/author/:author',async function (req, res) {
+    const theAuthor=req.params.author;
+    let theBook='';
+    try {
+        const theISBN = req.params.isbn;
+        const booksData = await new Promise((resolve, reject) => {
+            for (let key in books) {
+                if (books[key].author === theAuthor) {
+                    theBook =books[key];
+                }
+            }
+            resolve(theBook); 
+      });
+      res.status(200).json(booksData);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+/*
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const theAuthor=req.params.author;
@@ -78,6 +100,8 @@ public_users.get('/author/:author',function (req, res) {
     }
    res.status(200).json(theBook);
 });
+
+*/
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
