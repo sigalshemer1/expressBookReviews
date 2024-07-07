d2533d41b5254bcf79a8b51b1ea4f7122a3c8bcc
sigalshemer1 <sigal.shemer68@gmail.com>
2024-07-07 17:18:49 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,13 +67,12 @@ public_users.get('/', async function (req, res) {
     }
   });
 
-  
+
 // Get book details based on author
 public_users.get('/author/:author',async function (req, res) {
     const theAuthor=req.params.author;
     let theBook='';
     try {
-        const theISBN = req.params.isbn;
         const booksData = await new Promise((resolve, reject) => {
             for (let key in books) {
                 if (books[key].author === theAuthor) {
@@ -88,32 +87,26 @@ public_users.get('/author/:author',async function (req, res) {
     }
 });
 
-/*
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const theAuthor=req.params.author;
-    let theBook='';
-    for (let key in books) {
-        if (books[key].author === theAuthor) {
-            theBook =books[key];
-        }
-    }
-   res.status(200).json(theBook);
-});
-
-*/
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
     const theTitle=req.params.title;
     let theBook='';
-    for (let key in books) {
-        if (books[key].title === theTitle) {
-            theBook =books[key];
-        }
+    try {
+        const booksData = await new Promise((resolve, reject) => {
+            for (let key in books) {
+                if (books[key].title === theTitle) {
+                    theBook =books[key];
+                }
+            }
+            resolve(theBook); 
+      });
+      res.status(200).json(booksData);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
     }
-   res.status(200).json(theBook);
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
